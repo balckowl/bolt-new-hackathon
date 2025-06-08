1496f8599e5805e43443298d92ed05d8a5326632
PoPodada <goito.722@icloud.com>
2025-06-08 17:01:12 +0900

    fix: faviconが正常に描画されるように修正

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+	output: "export",
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		unoptimized: true,
+		domains: ["www.google.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "www.google.com",
+				port: "",
+				pathname: "/s2/favicons/**",
+			},
+		],
+	},
+};
 module.exports = nextConfig;
