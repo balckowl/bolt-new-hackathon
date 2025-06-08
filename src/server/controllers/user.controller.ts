b0f5a2369288ddca1770530581cdb058799d87cd
balckowl <shiggyjr1492@gmail.com>
2025-06-08 18:18:13 +0900

    feat: create api which is able to set os name

--- src/server/controllers/user.controller.ts
+++ src/server/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/src/lib/auth";
 import { prisma } from "@/src/lib/prisma";
 import type { RouteHandler } from "@hono/zod-openapi";
-import type { checkOsNameRoute } from "../routes/user.route";
+import type { checkOsNameRoute, setOsNameRoute } from "../routes/user.route";
 
 export const checkOsNameHandler: RouteHandler<typeof checkOsNameRoute> = async (c) => {
 	const { osName } = c.req.valid("json");
@@ -34,4 +34,43 @@ export const checkOsNameHandler: RouteHandler<typeof checkOsNameRoute> = async (
 	}
 
 	return c.json(null, 200);
+};
+
+export const setOsNameHandler: RouteHandler<typeof setOsNameRoute> = async (c) => {
+	const { osName } = c.req.valid("json");
+
+	const session = await auth.api.getSession({
+		headers: c.req.raw.headers,
+	});
+
+	if (!session?.user?.id) {
+		throw new Error("Unauthorized");
+	}
+
+	const me = await prisma.user.findUnique({
+		where: { id: session.user.id },
+		select: { osName: true },
+	});
+
+	if (!me) {
+		return c.json(null, 404);
+	}
+
+	if (me.osName !== null) {
+		return c.json(null, 400);
+	}
+
+	const exists = await prisma.user.findUnique({ where: { osName } });
+
+	if (exists) {
+		return c.json(null, 409);
+	}
+
+	await prisma.user.update({
+		where: { id: session.user.id },
+		data: { osName },
+		select: { osName: true },
+	});
+
+	return c.json(null, 201);
 };
