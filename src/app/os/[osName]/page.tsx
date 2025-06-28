6a3d6b5bca2ec7b41d316f9dc1d1fa007e1b91e0
PoPodada <goito.722@icloud.com>
2025-06-28 16:24:22 +0900

    fix: data type

--- src/app/os/[osName]/page.tsx
+++ src/app/os/[osName]/page.tsx
@@ -1,8 +1,6 @@
 import MacosDesktop from "@/src/components/MacosDesktop";
 import { hono } from "@/src/lib/hono-client";
-import type { desktopStateSchema } from "@/src/server/models/os.schema";
 import { headers } from "next/headers";
-import type z from "zod";
 
 export default async function Page({ params }: { params: { osName: string } }) {
 	const res = await hono.api.desktop[":osName"].$get(
@@ -18,10 +16,10 @@ export default async function Page({ params }: { params: { osName: string } }) {
 			},
 		},
 	);
-	if (!res.ok) {
-		return <div>Failed to get information</div>;
+	if (res.status === 404) {
+		return <div>No desktop information</div>;
 	}
-	const data: z.infer<typeof desktopStateSchema> = await res.json();
+	const data = await res.json();
 	return (
 		<>
 			<MacosDesktop desktop={data} />
