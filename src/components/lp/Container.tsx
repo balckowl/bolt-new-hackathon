c7a70ff5d2cadefd820d5afb4b2b20c6182aee63
balckowl <shiggyjr1492@gmail.com>
2025-09-27 13:45:36 +0900

    fix: 不要なファイルを削除

--- src/components/lp/Container.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-type Props = {
-	children: ReactNode;
-};
-
-export default function Container({ children }: Props) {
-	return <div className="mx-auto max-w-[800px]">{children}</div>;
-}
