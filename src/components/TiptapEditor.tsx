cd1fc1547b45c0f259caaee24346f9e5b3432c9a
PoPodada <goito.722@gmail.com>
2025-09-28 18:30:42 +0900

    fix: 開いたときにカーソルが当たるようにする

--- src/components/TiptapEditor.tsx
+++ src/components/TiptapEditor.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { type Editor, EditorContent, useEditor } from "@tiptap/react";
+import { useEffect } from "react";
 
 interface TiptapEditorProps {
 	editor: Editor | null;
 }
 
 export default function TiptapEditor({ editor }: TiptapEditorProps) {
+	useEffect(() => {
+		if (editor) {
+			editor.commands.focus();
+		}
+	}, [editor]);
+
 	if (!editor) {
 		return null;
 	}
